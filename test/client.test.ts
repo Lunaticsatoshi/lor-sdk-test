756ba1e677ae1c028405d9c443b5591dea6f3c5d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-18 20:18:08 +0530

    :test_tube: chore: updating test files

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,4 +1,3 @@
-import nock from 'nock';
 import { Client } from '../src';
 import { response } from './movies';
 
@@ -19,16 +18,15 @@ describe('test v2 client', () => {
     });
   })
 
-  test('rest api call', async () => {
-    nock('https://the-one-api.dev/v2', {
-      reqheaders: {
-        Authorization: 'test-api-key',
-      },
-    })
-      .get('/movies/')
-      .reply(200, JSON.parse(JSON.stringify(response)));
-
+  test('rest api call to get all movies', async () => {
     const res = await client.movies.getMovies();
+    expect(res).toHaveProperty('docs');
+    expect(res.docs).toBeDefined();
+  });
+
+  test('rest api call to get a single movie', async () => {
+    const res = await client.movies.getMovieById('5cd95395de30eff6ebccde5d');
+    expect(res).toEqual(response)
     expect(res).toHaveProperty('docs');
     expect(res.docs).toBeDefined();
   });
