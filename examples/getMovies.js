8ee9f46e7e5355760dd63b90d9f1f0867b6b70a7
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-02-18 19:43:47 +0530

    :zap: chore: adding examples

---
+++ examples/getMovies.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const {Client} = require('@lor-sdk-test/client');
+import { Client } from '@lunaticsatoshi/lor-sdk-test';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const dotenv = require('dotenv');
+
+// dotenv();
+
+// eslint-disable-next-line no-unexpected-multiline
+(async () => {
+  const client = new Client({
+    apiKey: '',
+  });
+
+  const response = await client.movies.getMovies();
+
+  console.log(JSON.parse(JSON.stringify(response, null, 4)));
+})();
